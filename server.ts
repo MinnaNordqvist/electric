912cbf47f3761730b60fb1ac1d284b9db6e69ba6
MinnaNordqvist <nordqvist.minna@gmail.com>
2026-09-03 16:01:05 +0300

    toimii

--- server.ts
+++ server.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { selectAll, selectSpecial } from './types.js';
+import { selectAll, selectSpecial } from './queries.js';
 
 const app = express();
 const PORT = 3000;
--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,22 @@
+import { useEffect, useState } from "react";
+
+
 export function App() {
-  return <h1>Electricity Dashboard</h1>;
+  const [data, setData] = useState<any[]>([]);  
+  useEffect(() => {
+     fetch('/api')
+    .then((res) => res.json())
+    .then((resData) => {
+        console.log('Fetched:', resData);
+        setData([...resData]); 
+      });
+    }, []);
+  
+  return(
+    <div>
+     <h1>Electricity Dashboard</h1>
+     <p>Data rows loaded: {data.length}</p>
+    </div>
+  )
+ 
 }
