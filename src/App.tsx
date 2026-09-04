a2c3f8faae9f1171e4efc31fd030fd05a6797e73
MinnaNordqvist <nordqvist.minna@gmail.com>
2026-09-04 11:57:55 +0300

    pagination

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,14 @@ import "./App.css"
 import { DataTable } from "./DataTable";
 
 export function App() {
-  const [data, setData] = useState<any[]>([]);  
-  useEffect(() => {
-     fetch('/api')
-    .then((res) => res.json())
-    .then((resData) => {
-        console.log('Fetched:', resData);
-        setData([...resData]); 
-      });
-    }, []);
+
   
   return(
     <>
     
     <div className="header">
     <h1>Electricity Dashboard</h1>
-    <p>Data rows loaded: {data.length}</p>
+  
     </div>
     
     <DataTable />
--- src/DataTable.tsx
+++ src/DataTable.tsx
@@ -1,23 +1,28 @@
-import { useEffect, useState } from "react";
-
-interface Electric {
-    date: string,
-    total_production: number,
-    total_consumption: number,
-    average_price: number,
-    longest_consecutive_negative_hours: number
-}
+import { useEffect, useState, useMemo } from "react";
 
 
 export function DataTable(){
-const [data, setData] = useState<any[]>([]);  
-  useEffect(() => {
-     fetch('/api')
-    .then((res) => res.json())
-    .then((resData) => {
-        setData([...resData]); 
-      });
+    const [data, setData] = useState<any[]>([]);  
+    const [currentPage, setCurrentPage] = useState<number>(1);
+    const pageSize = 10;
+
+
+    useEffect(() => {
+         fetch('/api')
+        .then((res) => res.json())
+        .then((resData) => {
+            setData([...resData]); 
+        });
     }, []);
+
+    const totalPages = Math.ceil(data.length / pageSize)
+    
+    const currentTableData = useMemo(() => {
+        const startIndex = (currentPage - 1) * pageSize;
+        return data.slice(startIndex, startIndex + pageSize);
+    }, [data, currentPage]);
+
+    const pageNumbers = Array.from({ length: totalPages }, (_, i) => i + 1);
 
 
 
@@ -28,14 +33,14 @@ const [data, setData] = useState<any[]>([]);
           <thead>
             <tr>
               <th>Date</th>
-              <th>Production (MWh/h)</th>
-              <th>Consumption (kWh)</th>
-              <th>Avg Price (snt/kWh)</th>
-              <th>Longest Negative Streak (h)</th>
+              <th>Total Production (MWh/h)</th>
+              <th>Total Consumption (kWh)</th>
+              <th>Avgerage Daily Price (snt/kWh)</th>
+              <th>Longest Negative Price Streak (h)</th>
             </tr>
           </thead>
           <tbody>
-            {data.map((row) => (
+            {currentTableData.map((row) => (
              <tr key={row.date}>
              <td>{row.date}</td>
              <td>{Number(row.total_production).toLocaleString()}</td>
@@ -46,6 +51,19 @@ const [data, setData] = useState<any[]>([]);
             ))}
           </tbody>
         </table>
+         <button 
+            onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
+            disabled={currentPage === 1}>
+            &laquo; Prev
+        </button>   
+        <button
+          onClick={() => setCurrentPage((prev) => Math.min(prev + 1, totalPages))}
+          disabled={currentPage === totalPages}>
+            Next &raquo;
+        </button>    
+
+
+
         </div>
         </>
     )
